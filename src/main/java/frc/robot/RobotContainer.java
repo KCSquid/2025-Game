f88662e19f0853a27320628994787c09b4744204
KCSquid <jahvonc9@gmail.com>
2025-03-09 17:31:04 -0400

    experimental createBinding function

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,7 +16,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.POVButton;
-
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.Elevator;
 import frc.robot.Constants.Keybindings;
 import frc.robot.Constants.Shooter;
@@ -28,85 +28,117 @@ import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class RobotContainer {
-        // subsystems
-        private final ElevatorSystem elevator = new ElevatorSystem(Elevator.M_ID_LEFT, Elevator.M_ID_RIGHT);
-        private final ShooterSubsystem shooter = new ShooterSubsystem(Shooter.M_ID_LEFT, Shooter.M_ID_RIGHT);
-        private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
+  // subsystems
+  private final ElevatorSystem elevator = new ElevatorSystem(Elevator.M_ID_LEFT, Elevator.M_ID_RIGHT);
+  private final ShooterSubsystem shooter = new ShooterSubsystem(Shooter.M_ID_LEFT, Shooter.M_ID_RIGHT);
+  private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
 
-        // controllers
-        private final XboxController controller = new XboxController(0);
+  // controllers
+  private final XboxController controller = new XboxController(0);
 
-        // motors
-        // TODO: channel
-        private final Servo servo = new Servo(0);
-        private boolean servoState = false;
+  // motors
+  private final Servo servo = new Servo(0);
+  private boolean servoState = false;
 
-        private int currentMax = 0;
-        private ArrayList<Integer> maxes = new ArrayList<Integer>() {
-                {
-                        add(0);
-                        add(50);
-                        add(100);
-                }
-        };
+  private int currentMax = 0;
+  private ArrayList<Integer> maxes = new ArrayList<Integer>() {
+    {
+      add(0);
+      add(50);
+      add(100);
+    }
+  };
 
-        // commands
-        public RobotContainer() {
-                // create named commands for pathplanner here
-                NamedCommands.registerCommand("Drop", new ShootNote(shooter, () -> 0.1));
+  // commands
+  public RobotContainer() {
+    // create named commands for pathplanner here
+    NamedCommands.registerCommand("Drop", new ShootNote(shooter, () -> 0.1));
 
-                swerveSubsystem.setDefaultCommand(new SwerveJoystickAuto(
-                                swerveSubsystem,
-                                () -> controller.getLeftY(),
-                                () -> controller.getLeftX(),
-                                () -> -controller.getRightY(),
-                                () -> -controller.getRightX()));
+    swerveSubsystem.setDefaultCommand(new SwerveJoystickAuto(
+        swerveSubsystem,
+        () -> controller.getLeftY(),
+        () -> controller.getLeftX(),
+        () -> -controller.getRightY(),
+        () -> -controller.getRightX()));
 
-                shooter.setDefaultCommand(new ShootNote(shooter, () -> controller.getRightTriggerAxis()));
+    shooter.setDefaultCommand(new ShootNote(shooter, () -> controller.getRightTriggerAxis()));
 
-                configureBindings();
+    configureBindings();
+  }
+
+  private enum BindingType {
+    BUTTON,
+    DPAD;
+  }
+
+  public Trigger createBinding(BindingType type, int button, Runnable trueAction) {
+    return createBinding(type, button, trueAction, () -> {
+    }, false);
+  }
+
+  public Trigger createBinding(BindingType type, int button, Runnable trueAction, Runnable falseAction) {
+    return createBinding(type, button, trueAction, falseAction, false);
+  }
+
+  public Trigger createBinding(BindingType type, int button, Runnable trueAction, Runnable falseAction,
+      boolean shouldRepeat) {
+    switch (type) {
+      case BUTTON:
+        if (shouldRepeat) {
+          return new JoystickButton(controller, button).whileTrue(new RepeatCommand(new InstantCommand(trueAction)))
+              .onFalse(new InstantCommand(falseAction));
+        } else {
+          return new JoystickButton(controller, button).onTrue(new InstantCommand(trueAction))
+              .onFalse(new InstantCommand(falseAction));
         }
-
-        // keybindings
-        private void configureBindings() {
-
-                new JoystickButton(controller, Keybindings.BUTTON_A).onTrue(new InstantCommand(() -> {
-                        servoState = !servoState;
-                        servo.setAngle(servoState ? 270 : -270);
-                }));
-
-                new JoystickButton(controller, Keybindings.BUTTON_B).onTrue(new InstantCommand(() -> {
-                        currentMax = (currentMax + 1) % maxes.size();
-                        elevator.setEncoderMax(maxes.get(currentMax));
-                        System.out.println(maxes.get(currentMax));
-                }));
-
-                // zero heading
-                new JoystickButton(controller, Keybindings.BUTTON_Y)
-                                .onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading()));
-
-                // configure DPAD
-                // elevator forward, backward -> start and stop
-                new POVButton(controller, Keybindings.DPAD_UP)
-                                .whileTrue(
-                                                new RepeatCommand(new InstantCommand(() -> elevator
-                                                                .setSpeed(Constants.Elevator.MOTOR_SPEED))))
-                                .onFalse(new InstantCommand(() -> elevator.lock()));
-                new POVButton(controller, Keybindings.DPAD_DOWN)
-                                .whileTrue(
-                                                new RepeatCommand(new InstantCommand(() -> elevator
-                                                                .setSpeed(-Constants.Elevator.MOTOR_SPEED))))
-                                .onFalse(new InstantCommand(() -> elevator.lock()));
-
-                // left bumper -> swerve joystick
-                new JoystickButton(controller, Keybindings.BUMPER_LEFT).whileTrue(new SwerveJoystickCmd(
-                                swerveSubsystem,
-                                () -> controller.getLeftY(),
-                                () -> -controller.getLeftX(),
-                                () -> -controller.getRightX()));
+      case DPAD:
+        if (shouldRepeat) {
+          return new POVButton(controller, button).whileTrue(new RepeatCommand(new InstantCommand(trueAction)))
+              .onFalse(new InstantCommand(falseAction));
+        } else {
+          return new POVButton(controller, button).onTrue(new InstantCommand(trueAction))
+              .onFalse(new InstantCommand(falseAction));
         }
+    }
+    return null;
+  }
 
-        public Command getAutonomousCommand() {
-                return new PathPlannerAuto("Drop It");
-        }
+  // keybindings
+  private void configureBindings() {
+
+    createBinding(BindingType.BUTTON, Keybindings.BUTTON_A,
+        () -> {
+          servoState = !servoState;
+          servo.setAngle(servoState ? 270 : -270);
+        });
+
+    createBinding(BindingType.BUTTON, Keybindings.BUTTON_B, () -> {
+      currentMax = (currentMax + 1) % maxes.size();
+      elevator.setEncoderMax(maxes.get(currentMax));
+      System.out.println(maxes.get(currentMax));
+    });
+
+    createBinding(BindingType.BUTTON, Keybindings.BUTTON_Y, () -> {
+      swerveSubsystem.zeroHeading();
+    });
+
+    // configure DPAD
+    // elevator forward, backward -> start and stop
+    createBinding(BindingType.DPAD, Keybindings.DPAD_UP, () -> elevator.setSpeed(Constants.Elevator.MOTOR_SPEED),
+        () -> elevator.lock(), true);
+
+    createBinding(BindingType.DPAD, Keybindings.DPAD_DOWN, () -> elevator.setSpeed(-Constants.Elevator.MOTOR_SPEED),
+        () -> elevator.lock(), true);
+
+    // left bumper -> swerve joystick
+    new JoystickButton(controller, Keybindings.BUMPER_LEFT).whileTrue(new SwerveJoystickCmd(
+        swerveSubsystem,
+        () -> controller.getLeftY(),
+        () -> -controller.getLeftX(),
+        () -> -controller.getRightX()));
+  }
+
+  public Command getAutonomousCommand() {
+    return new PathPlannerAuto("Drop It");
+  }
 }
